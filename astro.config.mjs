656fbefa248e532dc8abb97dd6a3dba6a47c5f65
RuslanOnoprienko2025 <ruslan.onopriienko@gmail.com>
2026-01-07 23:22:23 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    routes: {
+      strategy: 'include',
+      include: ['/*']
+    }
+  })
 });
