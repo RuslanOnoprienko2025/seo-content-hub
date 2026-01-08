64831576e122089ffc41fd8f72601b3d84116c52
RuslanOnoprienko2025 <ruslan.onopriienko@gmail.com>
2026-01-08 15:35:14 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,4 @@ export default defineConfig({
     // Добавь это:
     runtime: { mode: 'compatibility', type: 'pages' }
   }),
-  // Добавь это, чтобы Astro не конфликтовал со слэшами в конце URL
-  trailingSlash: 'always'
 });
