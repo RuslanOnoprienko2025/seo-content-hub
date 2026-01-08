cd7688a7e02d78cc21388a78c85526b38285e920
RuslanOnoprienko2025 <ruslan.onopriienko@gmail.com>
2026-01-08 02:39:19 +0200

    Delete functions/auth.js

--- functions/auth.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet(context) {
-  const { env } = context;
-  const client_id = env.GITHUB_CLIENT_ID;
-  
-  return Response.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user`
-  );
-}
