5f04aeb8d1f102a02d46d5c8b2e60fb52b7d1ea6
RuslanOnoprienko2025 <ruslan.onopriienko@gmail.com>
2026-02-04 23:55:22 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   trailingSlash: 'always', // Добавляем это для единого стандарта ссылок
   build: {
     format: 'directory',    // Добавьте эту строку
